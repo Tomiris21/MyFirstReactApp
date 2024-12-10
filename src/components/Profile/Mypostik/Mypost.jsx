5d9a8356abb25091a0ec86e4a2d9a7885dd3273f
Tomiris21 <zhandostomi19@gmail.com>
2024-12-11 02:56:43 +0500

    add

--- src/components/Profile/Mypostik/Mypost.jsx
+++ src/components/Profile/Mypostik/Mypost.jsx
@@ -23,10 +23,7 @@ const Mypost = (props) => {
         <button onClick={addPost}>Add</button>
       </div>
 
-      <div className={style.post}>
-        {postElements}
-        {postElements}
-      </div>
+      <div className={style.post}>{postElements}</div>
     </div>
   );
 };
